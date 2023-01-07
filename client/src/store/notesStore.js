fb4fac1c4b32b15f62eb99f7bda41208f76f8927
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-07 17:46:22 -0300

    create zustand store

--- client/src/store/notesStore.js
+++ client/src/store/notesStore.js
@@ -1,8 +1,8 @@
 import create from 'zustand'
 
-export const useNotesStore = create(() => ({
-  notes: {
-    title: '',
-    content: '',
-  },
+export const useNotesStore = create((set) => ({
+  notes: [],
+  createNote: (note) => set((state) => ({ ...state, notes: [...state.notes, note] })),
+  deleteNote: (id) =>
+    set((state) => ({ ...state, notes: [...state.notes.filter((note) => note.id !== id)] })),
 }))
