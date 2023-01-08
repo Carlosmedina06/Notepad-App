cd2e922253e9f5a706ee2e3ce49d972af8841476
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-08 02:37:06 -0300

    form container

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 
-const Form = ({ input, handleSubmit, handlerChange }) => {
+import { Container } from './FormStyle'
+
+const Form = ({ handleChange, handleSubmit, input }) => {
   return (
-    <>
+    <Container>
       <form onSubmit={handleSubmit}>
         <h2>Create your note</h2>
         <label htmlFor="title">Title</label>
-        <input id="title" name="title" type="text" value={input.title} onChange={handlerChange} />
+        <input id="title" name="title" type="text" value={input.title} onChange={handleChange} />
         <label htmlFor="content">Content</label>
         <input
           id="content"
           name="content"
           type="text"
           value={input.content}
-          onChange={handlerChange}
+          onChange={handleChange}
         />
         <button type="submit">Add Note</button>
       </form>
-    </>
+    </Container>
   )
 }
 
