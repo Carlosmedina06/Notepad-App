55675edb87b96cc60198ac59579a7404cacff19c
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-08 02:36:29 -0300

    create footer component

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { RxLinkedinLogo, RxGithubLogo } from 'react-icons/rx'
+
+import { Container, FooterContent, Footerheader } from './FooterStyle.js'
 
 const Footer = () => {
-  return <div>esto es un footer</div>
+  return (
+    <Container>
+      <Footerheader>
+        <h4>Made with ❤️ by Carlos Medina</h4>
+      </Footerheader>
+      <FooterContent>
+        <a href="https://github.com/carlosmedina06">
+          <RxGithubLogo />
+        </a>
+        <a href="https://www.linkedin.com/in/carlosmedina06/">
+          <RxLinkedinLogo />
+        </a>
+      </FooterContent>
+      <FooterContent>
+        <p> all rights reserved Carlos Medina® </p>
+      </FooterContent>
+    </Container>
+  )
 }
 
 export default Footer
