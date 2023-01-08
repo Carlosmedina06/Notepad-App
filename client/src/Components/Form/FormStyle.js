99b47b6cc10b983bf5874fa3de9193b8f8c38759
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-08 02:37:15 -0300

    form styles

---
+++ client/src/Components/Form/FormStyle.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 70%;
+  margin: 20px auto;
+  padding: 20px 50px;
+  background-color: #20232e;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  color: #fff;
+  border-radius: 10px;
+`
