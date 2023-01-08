39e74eeaf4794c4749ab8f9e2315217262162fe8
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-08 20:29:53 -0300

    notecard content and style

--- client/src/Components/NoteCard/NoteCard.jsx
+++ client/src/Components/NoteCard/NoteCard.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { RiArchiveLine, RiCheckboxLine } from 'react-icons/ri'
 
-import { Container } from './NoteCardStyle'
+import { Button, ButtonContainer, CardNote, Container, Content } from './NoteCardStyle'
 
 const NoteCard = ({ notes, handleDelete }) => {
   return (
     <Container>
       {notes.map((note) => {
         return (
-          <div key={note.id}>
-            <h2>{note.title}</h2>
-            <p>{note.content}</p>
-            <button onClick={() => handleDelete(note.id)}>Delete note</button>
-          </div>
+          <CardNote key={note.id}>
+            <Content>
+              <h2>{note.title}</h2>
+              <p>{note.content}</p>
+            </Content>
+            <ButtonContainer>
+              <Button onClick={() => handleDelete(note.id)}>
+                <RiCheckboxLine />
+              </Button>
+              <Button>
+                <RiArchiveLine />
+              </Button>
+            </ButtonContainer>
+          </CardNote>
         )
       })}
     </Container>
--- client/src/Components/NoteCard/NoteCardStyle.js
+++ client/src/Components/NoteCard/NoteCardStyle.js
@@ -1,3 +1,46 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.article`
+  margin: 5rem auto;
+  padding: 20px;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+`
+export const CardNote = styled.div`
+  display: flex;
+  margin: 0 auto;
+  min-width: 80%;
+  max-width: 80%;
+  min-height: 100px;
+  max-height: max-content;
+  border-radius: 10px;
+  color: #fff;
+  background-color: #20232e;
+  justify-content: space-between;
+`
+export const Content = styled.div`
+  display: flex;
+  width: 50%;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  margin: 20px;
+  text-align: start;
+`
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 1.2rem;
+  margin: 0 10px 0px 0;
+`
+export const Button = styled.button`
+  border: none;
+  background: transparent;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+`
