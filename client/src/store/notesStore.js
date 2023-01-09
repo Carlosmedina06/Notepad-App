8a67ed44e9609fefb5221194c77fe235e579b154
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-09 17:42:47 -0300

    zustand persist state in localStorage

--- client/src/store/notesStore.js
+++ client/src/store/notesStore.js
@@ -1,8 +1,17 @@
 import create from 'zustand'
+import { persist } from 'zustand/middleware'
 
-export const useNotesStore = create((set) => ({
-  notes: [],
-  createNote: (note) => set((state) => ({ ...state, notes: [...state.notes, note] })),
-  deleteNote: (id) =>
-    set((state) => ({ ...state, notes: [...state.notes.filter((note) => note.id !== id)] })),
-}))
+export const useNotesStore = create(
+  persist(
+    (set) => ({
+      notes: [],
+      createNote: (note) => set((state) => ({ ...state, notes: [...state.notes, note] })),
+      deleteNote: (id) =>
+        set((state) => ({ ...state, notes: [...state.notes.filter((note) => note.id !== id)] })),
+    }),
+    {
+      name: 'notes',
+      getStorage: () => localStorage,
+    },
+  ),
+)
