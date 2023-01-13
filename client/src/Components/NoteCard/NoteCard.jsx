21fa7d1d08fb1a1bc35ed22d0a51c9ba3a7d8270
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-13 04:26:11 -0300

    render loading when notes is undefined

--- client/src/Components/NoteCard/NoteCard.jsx
+++ client/src/Components/NoteCard/NoteCard.jsx
@@ -3,30 +3,34 @@ import { RiInboxArchiveLine, RiCloseFill, RiCheckFill } from 'react-icons/ri'
 
 import { Button, ButtonContainer, CardNote, Container, Content } from './NoteCardStyle'
 
-const NoteCard = ({ notes, handleDelete }) => {
+const NoteCard = ({ notes, handleDelete, handleCompleted }) => {
   return (
     <Container>
-      {notes.map((note) => {
-        return (
-          <CardNote key={note.id}>
-            <Content>
-              <h2>{note.title}</h2>
-              <p>{note.content}</p>
-            </Content>
-            <ButtonContainer>
-              <Button title="completed note">
-                <RiCheckFill />
-              </Button>
-              <Button title="delete note" onClick={() => handleDelete(note.id)}>
-                <RiCloseFill />
-              </Button>
-              <Button title="archive note">
-                <RiInboxArchiveLine />
-              </Button>
-            </ButtonContainer>
-          </CardNote>
-        )
-      })}
+      {notes === undefined ? (
+        <h1>loading...</h1>
+      ) : (
+        notes.map((note) => {
+          return (
+            <CardNote key={note.id}>
+              <Content>
+                <h2>{note.title}</h2>
+                <p>{note.content}</p>
+              </Content>
+              <ButtonContainer>
+                <Button title="completed note" onClick={() => handleCompleted(note.id)}>
+                  <RiCheckFill />
+                </Button>
+                <Button title="delete note" onClick={() => handleDelete(note.id)}>
+                  <RiCloseFill />
+                </Button>
+                <Button title="archive note">
+                  <RiInboxArchiveLine />
+                </Button>
+              </ButtonContainer>
+            </CardNote>
+          )
+        })
+      )}
     </Container>
   )
 }
