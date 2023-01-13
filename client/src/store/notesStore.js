10ea9a291a944ac485665c4033c5e599a55f5b9a
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-12 23:09:30 -0300

    fetch api

--- client/src/store/notesStore.js
+++ client/src/store/notesStore.js
@@ -1,10 +1,20 @@
 import create from 'zustand'
 import { persist } from 'zustand/middleware'
+const url = 'http://localhost:3001/notes'
 
 export const useNotesStore = create(
   persist(
     (set) => ({
       notes: [],
+      fetchNotes: () => {
+        fetch(url)
+          .then((res) => res.json())
+          .then((notes) =>
+            set({
+              notes: notes,
+            }),
+          )
+      },
       createNote: (note) => set((state) => ({ ...state, notes: [...state.notes, note] })),
       deleteNote: (id) =>
         set((state) => ({ ...state, notes: [...state.notes.filter((note) => note.id !== id)] })),
