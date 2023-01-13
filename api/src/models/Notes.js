5a9a75242fad478e60faf51a228f598af4133e89
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-12 22:34:59 -0300

    change noteScheme return object

--- api/src/models/Notes.js
+++ api/src/models/Notes.js
@@ -5,6 +5,14 @@ const noteSchema = new mongoose.Schema({
   content: String,
 })
 
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
 const Note = mongoose.model('Note', noteSchema)
 
 export default Note
