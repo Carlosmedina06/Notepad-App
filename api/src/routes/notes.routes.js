a199fbe4ebd309266fde564fd1e3cd69b15bfe63
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-13 04:25:14 -0300

    add controllers to routes

--- api/src/routes/notes.routes.js
+++ api/src/routes/notes.routes.js
@@ -1,57 +1,27 @@
 import { Router } from 'express'
 
-import Note from '../models/Notes.js'
+import {
+  createNote,
+  deleteNote,
+  getNotes,
+  getNotesById,
+  updateNote,
+} from '../controllers/notes.controllers.js'
+import { errorHandler } from '../middlewares/errorHandler.js'
+import { validateNote } from '../middlewares/validation.js'
 
 const notesRouter = Router()
 
-notesRouter.get('/', async (req, res) => {
-  const notes = await Note.find({})
+notesRouter.get('/', getNotes)
 
-  notes.length ? res.status(200).json(notes) : res.status(404).send({ message: 'No notes found' })
-})
+notesRouter.get('/:id', getNotesById)
 
-notesRouter.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params
+notesRouter.post('/', validateNote, createNote)
 
-    const note = await Note.findById(id)
+notesRouter.put('/:id', updateNote)
 
-    note ? res.status(200).json(note) : res.status(404).send({ message: 'Note not found' })
-  } catch (error) {
-    next(error)
-  }
-})
+notesRouter.delete('/:id', deleteNote)
 
-notesRouter.post('/', async (req, res) => {
-  const { title, content } = req.body
-
-  const newNote = new Note({
-    title,
-    content,
-  })
-
-  const noteSaved = await newNote.save()
-
-  res.status(201).json(noteSaved)
-})
-
-notesRouter.delete('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params
-    const noteDeleted = await Note.findByIdAndDelete(id)
-
-    noteDeleted
-      ? res.status(204).send({ message: 'Note Deleted' })
-      : res.status(404).send({ message: 'Note not found' })
-  } catch (error) {
-    next(error)
-  }
-})
-
-notesRouter.use((error, req, res, _next) => {
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return res.status(400).send({ message: 'Malformatted id' }).end()
-  }
-})
+notesRouter.use(errorHandler)
 
 export default notesRouter
