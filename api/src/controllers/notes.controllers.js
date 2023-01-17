7c9912d01412217d0ffa45d4e48b38e4021cdeb4
carlosmedina06 <slaawmedina14@gmail.com>
2023-01-17 19:48:01 -0300

    fix tiped error

--- api/src/controllers/notes.controllers.js
+++ api/src/controllers/notes.controllers.js
@@ -6,7 +6,7 @@ export const getNotes = async (req, res) => {
 
     notes ? res.status(200).json(notes) : res.status(404).send([{ message: 'No notes founddasda' }])
   } catch (error) {
-    res.status(500).send({ message: 'hp;a' })
+    res.status(500).send({ message: 'notes not found' })
   }
 }
 
